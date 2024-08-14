5ff1f89f0a7570a91272ef4d79bdf4cd193bc8af
crestharpan <arpanshrestha225@gmail.com>
2024-08-14 22:23:53 +0545

    own module import

--- nodeFarm/server.js
+++ nodeFarm/server.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
+const fs = require("fs"); // core modules
 const url = require("url");
 const http = require("http");
+//importing own built modules
+const replaceTempHtml = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -17,20 +19,6 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const replaceTempHtml = function (temp, product) {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRODUCTNUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
 //server
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
