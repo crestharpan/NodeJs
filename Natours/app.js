437f1198ea9d9ad382d921eb07e9bebea6fdf6cf
crestharpan <arpanshrestha225@gmail.com>
2025-01-22 10:39:31 +0545

    intial commit

--- Natours/app.js
+++ Natours/app.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server side', app: 'Natours' });
-// });
+app.use(express.json());
 
 //route
-//reading the tour data
+//reading the tour data from dev-data
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -20,6 +17,23 @@ app.get('/api/V1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+//getting the req from client
+app.post('/api/V1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      res.status(201).json({
+        status: 'successful',
+        data: newTour,
+      });
+    }
+  );
 });
 
 const port = 8000;
