db84dc6400e343213fb11d9d79db659de146e742
crestharpan <arpanshrestha225@gmail.com>
2025-01-27 17:23:25 +0545

    added mongoDB

--- Natours/server.js
+++ Natours/server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const Db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(Db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection Successful'));
 //START SERVERS
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
