9b7e125230f6cc563f94dd385f9d88e8d0e2b5a4
crestharpan <arpanshrestha225@gmail.com>
2025-01-29 20:16:17 +0545

    Build a complete model

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -4,16 +4,54 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A true must have duration!'],
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
     unique: true,
   },
-  rating: {
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have total number of people'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must specify difficulty level'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a descriptions'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a imgae'],
+  },
+  //will stores the array of references to the images
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
