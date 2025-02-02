052f8fa842f21aa9ee558bc5d6838cb4fde25a1d
crestharpan <arpanshrestha225@gmail.com>
2025-02-02 17:11:13 +0545

    Refactored

--- Natours/Controllers/tourController.js
+++ Natours/Controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasRoute = (req, res, next) => {
   req.query.limit = '5';
@@ -6,58 +7,6 @@ exports.aliasRoute = (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,difficulty,summary';
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-
-    excludeFields.forEach((element) => {
-      delete queryObj[element];
-    });
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-    return this;
-  }
-
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v'); //Excluding the __v
-    }
-    return this;
-  }
-
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    this.query = this.query.skip(skip).limit(limit);
-
-    return this;
-  }
-}
 
 // ROUTE HANDLERS
 exports.getTours = async (req, res) => {
