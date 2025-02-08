7062f008d2d0174884232f6db61f73eae5042dc0
crestharpan <arpanshrestha225@gmail.com>
2025-02-08 20:15:20 +0545

    prod-dev-erros-handling

--- Natours/Controllers/errorController.js
+++ Natours/Controllers/errorController.js
@@ -8,10 +8,24 @@ const sendErrDev = (err, res) => {
 };
 
 const sendErrPro = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //OPERATIONA ERROR: SEND MESSAGE TO THE CLIENT
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //OTHER UNKNOW PROGRAMMING ERRORS: DON'T GIVE DETAILS ABOUT THE ERROR TO THE CLIENT
+  } else {
+    //LOG THE ERROR (FOR US TO IDENTIFY THE ERROR)
+    console.error('ERROR:', err);
+
+    //SEND THE GENERIC MESSAGE ERROR TO THE CLIENT
+    res.status(500).json({
+      status: 'Fail',
+      message: 'Something went Wrong',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
