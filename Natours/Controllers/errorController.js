31ab92f4b4a3898540a3fee3bd76057d8286055b
crestharpan <arpanshrestha225@gmail.com>
2025-02-08 19:58:59 +0545

    error-manage

--- Natours/Controllers/errorController.js
+++ Natours/Controllers/errorController.js
@@ -1,9 +1,26 @@
-module.exports = (err, req, res, next) => {
-  console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message,
+  });
+};
+
+const sendErrPro = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'Error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrPro(err, res);
+  }
 };
