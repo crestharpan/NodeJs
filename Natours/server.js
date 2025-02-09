d36c0102d1bfc122336cd3c6791f62ab6b7366e5
crestharpan <arpanshrestha225@gmail.com>
2025-02-09 20:21:52 +0545

    handling-uncaughtExceptions

--- Natours/server.js
+++ Natours/server.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+//HANDLING UNCAUGHT EXCEPTIONS
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT REJECTION--Shutting the application');
+
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const Db = process.env.DATABASE.replace(
@@ -13,6 +22,16 @@ mongoose.connect(Db).then(() => console.log('DB connection Successful'));
 
 //START SERVERS
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+//HANDLING THE REJECTED PROMISES
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION--Shutting the application');
+  //SHUT DOWN THE APPLICATION ON FAILS DB CONNECTION
+  server.close(() => {
+    process.exit(1);
+  });
 });
