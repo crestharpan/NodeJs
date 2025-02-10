21c60a7e9747679630e1d7084dbe0581a002bf4d
crestharpan <arpanshrestha225@gmail.com>
2025-02-10 19:50:58 +0545

    install-JWT

--- Natours/Controllers/authController.js
+++ Natours/Controllers/authController.js
@@ -3,7 +3,12 @@ const User = require('../models/usersModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(200).json({
     status: 'Successfully Created',
