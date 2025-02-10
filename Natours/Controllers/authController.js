f6d4976a34b6651e025d75ecc3d1ee132f0081ea
crestharpan <arpanshrestha225@gmail.com>
2025-02-10 20:17:02 +0545

    created-JWT-token

--- Natours/Controllers/authController.js
+++ Natours/Controllers/authController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/usersModel');
 
 const catchAsync = require('../utils/catchAsync');
@@ -9,9 +11,13 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(200).json({
     status: 'Successfully Created',
+    token,
     data: {
       User: newUser,
     },
