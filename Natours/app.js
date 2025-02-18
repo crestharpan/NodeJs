87ae130d0c1349651f6b39d4f07ec144aa0342a5
crestharpan <arpanshrestha225@gmail.com>
2025-02-18 21:16:51 +0545

    headers security set

--- Natours/app.js
+++ Natours/app.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const AppError = require('./utils/appError');
 const globalErrorHandler = require('./Controllers/errorController');
 //importing the routes module
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 //GLOBAL MIDDLEWARE
+app.use(helmet()); //SET SECURITY HTTP HEADERS
 
+//DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //it will return the req
 }
 
+//LIMIT THE REQUEST FROM THE SAME IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from the same ip, Try again in 1 hour',
 });
+app.use('/api', limiter); //TO USE LIMITER ON ALL ROUTES FOR /API
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public/overview.html`)); //serving static files
+//BODY PARSER(READING DATA FROM BODY inTO REQ.BODY)
+app.use(express.json({ limit: '10KB' }));
 
+//serving static files
+app.use(express.static(`${__dirname}/public/overview.html`));
+
+//TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); //manipulating the req
   next();
 });
 
 // ROUTES
-app.use('/api', limiter); //TO USE LIMITER ON ALL ROUTES FOR /API
 app.use('/api/V1/tours', tourRouter);
 app.use('/api/V1/users', userRouter);
 
