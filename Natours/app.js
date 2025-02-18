e28c882ff82dad6c2a4bc4ba320ef947c362b87d
crestharpan <arpanshrestha225@gmail.com>
2025-02-18 21:48:53 +0545

    handled PPM

--- Natours/app.js
+++ Natours/app.js
@@ -3,6 +3,10 @@ const app = express();
 const morgan = require('morgan');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const mongoSantitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./Controllers/errorController');
 //importing the routes module
@@ -29,7 +33,24 @@ app.use('/api', limiter); //TO USE LIMITER ON ALL ROUTES FOR /API
 app.use(express.json({ limit: '10KB' }));
 
 //DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSantitize());
+
 //DATA SANTIZATION AGAINST CROSS-SITE ATTACK
+app.use(xss());
+
+//PREVENT PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public/overview.html`));
