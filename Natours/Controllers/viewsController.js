40290a7d108b9c6fe2c00b0d58d26562c4146216
crestharpan <arpanshrestha225@gmail.com>
2025-03-10 22:13:29 +0545

    overview page

--- Natours/Controllers/viewsController.js
+++ Natours/Controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) GET THE TOURS FROM THE COLLECTION
+  const tours = await Tour.find();
+  //2) BUILD THE TEMPLATE
+  //3) RENDER THE TEMPLATE USING THE DATA
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 exports.tour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
