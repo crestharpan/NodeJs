27d69c02804247fb92dec24251cd1291aca7ce66
crestharpan <arpanshrestha225@gmail.com>
2025-03-15 21:17:37 +0545

    the-tour-detail-page-setup

--- Natours/Controllers/viewsController.js
+++ Natours/Controllers/viewsController.js
@@ -11,8 +11,12 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     tours,
   });
 });
-exports.tour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  res.status(200).render('tour', {
+    data: tour,
+  });
+});
--- Natours/routes/viewRoutes.js
+++ Natours/routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewsController = require('../Controllers/viewsController');
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.tour);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
