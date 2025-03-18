0df6ea70303dc9c377d0c312b53bbc7a6fb5c335
crestharpan <arpanshrestha225@gmail.com>
2025-03-18 21:45:53 +0545

    mapbox setup

---
+++ Natours/public/js/mapbox.js
@@ -0,0 +1,4 @@
+/*eslint-disable*/
+console.log('Hello from client');
+const locations=JSON.parse(document.getElementById('map').dataset.locations); //converting the string to json
+console.log(locations);
