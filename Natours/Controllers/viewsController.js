594ce746c48d797c1115d556a140058ac1b9820a
crestharpan <arpanshrestha225@gmail.com>
2025-03-19 22:00:58 +0545

    added a login page

--- Natours/Controllers/viewsController.js
+++ Natours/Controllers/viewsController.js
@@ -21,3 +21,10 @@ exports.getTour = catchAsync(async (req, res) => {
     tour
   });
 });
+
+
+exports.getLoginForm=catchAsync(async(req,res)=>{
+res.status(200).render('login',{
+  title:'login'
+})
+})
