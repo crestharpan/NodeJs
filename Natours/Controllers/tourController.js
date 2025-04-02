6dd75eba70ac1ccfcc2c5f4d6986d6c16fb48750
crestharpan <arpanshrestha225@gmail.com>
2025-04-02 21:56:27 +0545

    multer configuretion for the tour images

--- Natours/Controllers/tourController.js
+++ Natours/Controllers/tourController.js
@@ -1,7 +1,35 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Invalid file type', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+  {
+    name: 'imageCover',
+    maxCount: 1,
+  },
+  {
+    name: 'images',
+    maxCount: 3, //The maxcount determines the number of files the field can accept
+  },
+]);
 
 exports.aliasRoute = (req, res, next) => {
   req.query.limit = '5';
