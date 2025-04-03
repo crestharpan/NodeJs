432944144637e609a3971b3f96c951eee147343e
crestharpan <arpanshrestha225@gmail.com>
2025-04-03 21:14:11 +0545

    imageCover update funcitonality

--- Natours/Controllers/tourController.js
+++ Natours/Controllers/tourController.js
@@ -35,11 +35,16 @@ exports.resizeTourImages = catchAsync(async (req, res, next) => {
   if (!req.files.imageCover || !req.files.images) {
     return next();
   }
-  await sharp(req.file.buffer)
-    .resize(500, 500)
+
+  //1) COVER IMAGE
+  const imageCoverFileName = `tour-${req.params.id}=${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+    .toFile(`public/img/tours/${imageCoverFileName}`);
+  req.body.imageCover = imageCoverFileName;
+  next();
 });
 
 exports.aliasRoute = (req, res, next) => {
