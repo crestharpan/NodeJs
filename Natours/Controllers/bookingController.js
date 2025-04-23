93831371868754a4a34ca5bc327439c60e529fd6
crestharpan <arpanshrestha225@gmail.com>
2025-04-23 21:28:00 +0545

    refactoring the booking controllers

--- Natours/Controllers/bookingController.js
+++ Natours/Controllers/bookingController.js
@@ -1,10 +1,16 @@
 const Booking = require('../models/bookingsModel');
-const catchAsync = require('../utils/catchAsync');
+// const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
-exports.createBooking = catchAsync(async (req, res) => {
-  await Booking.create(req.body);
-  res.status(200).json({
-    status: 'success',
-    message: 'Booked',
-  });
-});
+// exports.createBooking = catchAsync(async (req, res) => {
+//   await Booking.create(req.body);
+//   res.status(200).json({
+//     status: 'success',
+//     message: 'Booked',
+//   });
+// });
+exports.createBooking = factory.createOne(Booking);
+exports.getAll = factory.getAll(Booking);
+exports.getOne = factory.getOne(Booking);
+exports.updateOne = factory.updateOne(Booking);
+exports.deleteOne = factory.deleteOne(Booking);
--- Natours/routes/bookingRoutes.js
+++ Natours/routes/bookingRoutes.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const authController = require('../Controllers/authController');
 const bookingController = require('../Controllers/bookingController');
 
 const router = express.Router();
 
-router.post('/bookTour', bookingController.createBooking);
+router.use(authController.protect);
+
+router.post('/bookTour', bookingController.createOne);
+
+router.use(authController.restrictTO('admin', 'lead-guide'));
+
+router.get('/getBookings', bookingController.getAll);
+router
+  .route('/:id')
+  .get(bookingController.getOne)
+  .patch(bookingController.updateOne)
+  .post(bookingController.deleteOne);
+
 module.exports = router;
