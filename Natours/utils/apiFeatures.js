2d8a8d2b25d6deee5ce67b20b97a4830d8c4aaf2
crestharpan <arpanshrestha225@gmail.com>
2025-11-26 19:11:17 +0545

    removing the consoles

--- Natours/utils/apiFeatures.js
+++ Natours/utils/apiFeatures.js
@@ -16,13 +16,13 @@ class APIFeatures {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     this.query = this.query.find(JSON.parse(queryStr));
-
     return this;
   }
 
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log('Here is the sortBy:', sortBy);
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
