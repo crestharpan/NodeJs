345b588b6bc6d7388381445cd73b0272d7c87de2
crestharpan <arpanshrestha225@gmail.com>
2025-12-09 19:10:35 +0545

    updating the port

--- Natours/server.js
+++ Natours/server.js
@@ -23,7 +23,7 @@ mongoose.connect(Db).then(() => console.log('DB connection Successful'));
 //START SERVERS
 const port = process.env.PORT;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port- ${port}...`);
 });
 
 console.log('NODE_ENV:', process.env.NODE_ENV);
